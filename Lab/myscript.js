be922302b75d8436e3904beab5b22e5d6264a50a
perlah <herrp830@newschool.edu>
2015-10-23 18:51:54 +0000

    msg ...

---
+++ Lab/myscript.js
@@ -0,0 +1,22 @@
+console.log("It works");
+
+var excite = "!";
+var mark = "?";
+var sign = "$"
+var i = 0;
+var x = 20;
+var y = 5
+
+for ( i = 0; i <= 20; i++) {
+console.log(mark="?");
+mark = mark + "?";
+    for(x = 20; x <= 30; x++) {
+        console.log(excite="!");
+        excite = excite + "/!/!"
+            if(y <= 20){
+            console.log(sign);
+            sign = sign + "$";
+            }
+    }
+}
+
