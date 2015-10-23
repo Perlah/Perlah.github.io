5fc046b49a276a7990f7ce2118a82548a79a4ed9
perlah <herrp830@newschool.edu>
2015-10-23 19:29:06 +0000

    msg ...

--- Lab/myscript.js
+++ Lab/myscript.js
@@ -1,22 +1,33 @@
 console.log("It works");
 
-var excite = "!";
+var excite = "!!!!!!!!!!!!!!!!!░ ▒ ▓ ░ ▒ ▓ ░ ▒ ▓ ";
 var mark = "?";
-var sign = "$"
+
+
+var sign = "░ ▒ ▓ "
+var line = "########"
 var i = 0;
 var x = 20;
 var y = 5
+var array = [5,10,15];
 
 for ( i = 0; i <= 20; i++) {
 console.log(mark="?");
 mark = mark + "?";
-    for(x = 20; x <= 30; x++) {
-        console.log(excite="!");
-        excite = excite + "/!/!"
-            if(y <= 20){
+    for(x=20;x < 30; x++) {
+        console.log(excite="!!!!!!!!!!!!!!!!!!");
+        excite = excite + "!!!!!!!!!!!!!!!!!!"
+    //    if (array = 5,10,15){
+    //console.log();
+    //line = line + "########"
+    //}else
+    if(y <= 20){
             console.log(sign);
-            sign = sign + "$";
-            }
+            
+            
+            sign = sign + "░ ▒ ▓ ";
+      }
     }
 }
+
 
